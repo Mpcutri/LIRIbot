7814d4a1a59321f3a0fc634282f124e4ee0ec65d
Maxwell Cutri <mpcutri@gmail.com>
2018-02-15 10:04:00 -0700

    fully functioning

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 // console.log(process.env); // environment variables
 
 exports.twitter = {
--- liri.js
+++ liri.js
@@ -6,14 +6,12 @@ var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 var request = require('request');
 var userSearch = process.argv[3]; // Either <song name> or <movie name>
-	// if (userSearch.length == 0) {
-	// 	userSearch = "Mr. Nobody";
-	// }
 var operator = process.argv[2];
 var omdbURL = "http://www.omdbapi.com/?t=" + userSearch + "&apikey=trilogy";
-// REQUEST OMDB NPM ================================================================
 
+	
 switch(operator) {
+	// TWITTER NPM ================================================================
 	case "my-tweets":
 		var client = new Twitter({
 		  consumer_key: keys.twitter.consumer_key,
@@ -31,34 +29,30 @@ switch(operator) {
 		  }
 		});
 		break;
+	// SPOTIFY NPM ================================================================
 	case "spotify-this-song":
 		var spotify = new Spotify({
 			id: process.env.SPOTIFY_ID,
 			secret: process.env.SPOTIFY_SECRET
 		});
-		console.log(userSearch);
-		spotify.search({ type: 'track', query: userSearch }, function(err, data) {
+		spotify.search({ type: 'track', query: userSearch, limit: 1 }, function(err, data) {
 			if (err) {
 				return console.log('Error occurred: ' + err);
 			} else {
-				console.log(data.tracks[0]);
-				console.log("Title: " + body.Title);
-				console.log("Year Released: " + body.Year);
-				console.log("IMDB Rating: " + JSON.stringify(body.Ratings[0].Value));
-				console.log("Rotten Tomatoes Rating: " + JSON.stringify(body.Ratings[1].Value));
-				console.log("Language: " + body.Language);
-				console.log("Plot: " + body.Plot);
-				console.log("Actors: " + body.Actors);
+				console.log(data.tracks.items[0].artists[0].name);
+				console.log(data.tracks.items[0].name);
+				console.log(data.tracks.items[0].preview_url);
+				console.log(data.tracks.items[0].album.name);
 			};
 		})
 		break;
+	// REQUEST OMDB NPM ================================================================
 	case "movie-this":
 		request(omdbURL, function (error, response, body) {
-			// console.log('error:', error); // Print the error if one occurred
-			// console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-			// console.log('body:', body); // Print the HTML for the Google homepage.
-			// console.log(JSON.parse(response.body, null, 2)); // HOW DO I CLEAN UP THE CODE IN TERMINAL?
 			body = JSON.parse(body);
+			// if (userSearch == "") {
+			// 	userSearch = "Mr. Nobody"; // Not quite working yet.
+			// }
 
 			if (!error && response.statusCode === 200) {
 				console.log("Title: " + body.Title);
@@ -76,7 +70,3 @@ switch(operator) {
 		break;
 }
 
-
-// PUT THIS INSIDE OF A FUNCTION LIKE 
-// console.log(JSON.stringify(data, null, 2));
-// console.log(err);
